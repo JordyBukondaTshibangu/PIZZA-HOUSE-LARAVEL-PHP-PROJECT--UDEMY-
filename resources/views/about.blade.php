d9e81cb771732d4c98ce10723c678a6c97127e04
Jordy Tshibangu <jordytshibss@gmail.com>
2021-03-25 15:48:16 +0200

    feat : adding content to the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,22 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                <h1> Laravel project</h1>
-               <p> This is a laravel project
-                   created by Jordy Tshibss  
+                <h3 class="mt-5"> 
+                    This is a small blog article app created with the help of Laravel and its default blade template.
+                </h3>
+                <p>
+                    This app perfoms the full CRUD operation.
+                    It implements the default laravel authentication system.
                 </p>
+
+                <div class="content">
+                    <div class="title m-b-md">
+                       <h3 class="mt-5">Hi, I am  Jordy Tshibangu, and I am an ambious junior fullstack software developer who likes to work and learn new technologies
+                        whenever possible</h3>
+                    </div>
+                    If you would like to see more projects with other tech,
+                    Please click <a href="https://github.com/JordyBukondaTshibangu?tab=repositories" target="blank">here </a>
+                </div>
             </div>
         </div>
 
